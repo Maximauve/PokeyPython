277dff1fc5462f5ced34bc05b0a241c9901c0570
Maxime Mourgues <maxime.mourgues@ynov.com>
2021-02-03 17:27:29 +0100

    Ending TERMINÉ

--- package/Ending.py
+++ package/Ending.py
@@ -2,8 +2,12 @@ from .Shuffle import table
 from .Game import Whole_Game
 g = Whole_Game()
 
-# td Ne pas faire de print dans les fonctions de vérifications de cartes --> Ne retourner que la valeur
-# td Les prints devront se faire par rapport à la valeure retournée (ex: si c'est un 5 qui est retourné, alors on sait que c'est une paire de 6)
+
+#! Return des fonctions:
+# * Value[0] --> Retourne le nombre de points rapportés par la valeure
+# * Value[1] --> Retourne la valeure gagnée
+# * Value[2] --> Retourne le message
+#   Value[3] --> Complément (optionnel)
 
 
 class EndGame:
@@ -17,8 +21,6 @@ class EndGame:
         for i in range(2):
             check.append(player.hand[i])
         # * Debug
-        print(check)
-        print('\n')
         return check
 
     def NumberCheck(self, deck):
@@ -33,190 +35,114 @@ class EndGame:
             power.append(deck[a][1])
         return power
 
-    def Paire(self, player):
-        number = self.NumberCheck(self.CheckCards(player))
-        for i in range(len(number)-1):
-            for y in range(len(number)-1):
-                if number[i] - number[y] == 0:
-                    if i != y:
-                        print(
-                            f"Vous avez une paire de {number[i]} !")
-                        if number[i] == 1:
-                            return 13
-                        else:
-                            return number[i] - 1
+    def Paire(self, player, number=None):
+        if number == None:
+            number = self.NumberCheck(self.CheckCards(player))
+        numbersort = sorted(number, reverse=True)
+        if numbersort[5:] == [1, 1]:
+            numbersort[5:], numbersort[:5] = numbersort[:5], numbersort[5:]
+            return 13, 1
+        for i in range(len(numbersort)):
+            if i+1 >= len(numbersort):
+                break
+            if numbersort[i] == numbersort[i+1]:
+                return (numbersort[i] - 1), numbersort[i], f"Vous avez une Paire de {numbersort[i]}", i
+        return None
 
     def DoublePaire(self, player):
         number = self.NumberCheck(self.CheckCards(player))
-        dblnumber = number
-        pair = []
-        poped = False
-        for nb in range(2):
-            if nb == 1:
-                poped = False
-            for i in range(len(dblnumber)):
-                for y in range(len(dblnumber)):
-                    if dblnumber[i] - dblnumber[y] == 0:
-                        if i != y:
-                            if i > y:
-                                pair.append(dblnumber[i])
-                                dblnumber.pop(i)
-                                dblnumber.pop(y)
-                                poped = True
-                            else:
-                                pair.append(dblnumber[i])
-                                dblnumber.pop(y)
-                                dblnumber.pop(i)
-                                poped = True
-                            break
-                if poped == True:
-                    break
-        if len(dblnumber) == 3:
-            poped = False
-            for i in range(len(dblnumber)):
-                for y in range(len(dblnumber)):
-                    if dblnumber[i] - dblnumber[y] == 0:
-                        if i != y:
-                            if i > y:
-                                pair.append(dblnumber[i])
-                                dblnumber.pop(i)
-                                dblnumber.pop(y)
-                                poped = True
-                            else:
-                                pair.append(dblnumber[i])
-                                dblnumber.pop(y)
-                                dblnumber.pop(i)
-                                poped = True
-                            break
-                if poped == True:
-                    break
-            if len(pair) == 3:
-                pair.sort()
-                if pair[0] == 1:
-                    pair[0], pair[1], pair[2] = pair[1], pair[2], pair[0]
-                print(
-                    f"Vous avez deux paires ! Une paire de {pair[1]} et de {pair[2]}")
-                if pair[2] == 1:
-                    pair[2] = 14
-                return pair[2] + 11
-            elif len(pair) == 2:
-                pair.sort()
-                if pair[0] == 1:
-                    pair[0], pair[1] = pair[1], pair[0]
-                print(
-                    f"Vous avez deux paire ! Une paire de {pair[0]} et de {pair[1]}")
-                if pair[1] == 1:
-                    pair[1] = 14
-                return pair[1] + 11
-            else:
-                return 0
-        else:
-            return 0
+        numberpop = sorted(number, reverse=True)
+        if numberpop[5:] == [1, 1]:
+            numberpop[5:], numberpop[:5] = numberpop[:5], numberpop[5:]
+        if self.Paire(player):
+            for _ in range(2):
+                numberpop.pop(self.Paire(player)[3])
+            res = self.Paire(player, numberpop)
+            if res:
+                return self.Paire(player)[0] + 12, [self.Paire(player)[1], res[1]], f"Doube Paire de {self.Paire(player)[1]} et de {res[1]}"
+            return None
 
     def Brelan(self, player):
         number = self.NumberCheck(self.CheckCards(player))
-        numbertrié = number
-        brelan = []
-        poped = False
-        for nb in range(2):
-            if nb == 1:
-                poped = False
-            for i in range(len(numbertrié)):
-                for y in range(len(numbertrié)):
-                    for x in range(len(numbertrié)):
-                        if numbertrié[i] == numbertrié[y]:
-                            if numbertrié[i] == numbertrié[x]:
-                                if i != y and i != x and y != x:
-                                    brelan.append(numbertrié[i])
-                                    tab = [x, y, i]
-                                    tab.sort()
-                                    numbertrié.pop(tab[2])
-                                    numbertrié.pop(tab[1])
-                                    numbertrié.pop(tab[0])
-                                    poped = True
-                                    if nb == 1:
-                                        continue
-                                    break
-                    if poped == True:
-                        break
-                if poped == True:
+        numbertrié = sorted(number, reverse=True)
+        if numbertrié[4:] == [1, 1, 1]:
+            numbertrié[4:], numbertrié[:4] = numbertrié[:4], numbertrié[4:]
+        brelan = 0
+        for i in range(len(numbertrié)-2):
+            if numbertrié[i] - numbertrié[i+1] == 0:
+                if numbertrié[i+1] - numbertrié[i+2] == 0:
+                    brelan = numbertrié[i]
                     break
-        brelan.sort()
-        if len(brelan) > 1:
-            if brelan[0] > brelan[1]:
-                brelan[0], brelan[1] = brelan[1], brelan[0]
-            if brelan[1] == 1:
-                brelan[1] = 14
-            return brelan[1] + 24
-        elif len(brelan) > 0:
-            if brelan[0] == 1:
-                brelan[0] = 14
-            return brelan[0] + 24
-        else:
-            return 0
+        if brelan:
+            return brelan + 24, brelan, f"Vous avez un Brelan de {brelan} !", i
+
+        return None
 
     def Full(self, player):
 
-        Brelan = self.Brelan(player)
-        Paire = self.Paire(player)
-        if Brelan != 0:
-            if Paire != 0:
-                print("Vous avez un Full !")
-                return Brelan + 13
-        else:
-            return 0
-
-    def Suite(self,player):
         number = self.NumberCheck(self.CheckCards(player))
-        numbertrié = number
-        numbertrié.sort()
+        numberpop = sorted(number, reverse=True)
+        if self.Brelan(player):
+            for _ in range(3):
+                numberpop.pop(self.Brelan(player)[3])
+            if self.Paire(player, numberpop):
+                return self.Brelan(player)[1] + 13, [self.Brelan(player)[1], self.Paire(player, numberpop)[1]], f"Vous avez un full! Brelan de {self.Brelan(player)[1]} et Paire de {self.Paire(player, numberpop)[1]}"
+            return None
+
+    def Suite(self, player, number=None):
+        if number == None:
+            number = self.NumberCheck(self.CheckCards(player))
+        numbertrié = sorted(number, reverse=True)
         tab = []
         suite = []
+        compt = 0
         for i in range(len(numbertrié)-1):
             if numbertrié[i+1] == numbertrié[i]:
                 tab.append(i)
-        for x in tab:
-            numbertrié.pop(x)
-        if numbertrié[0] == 1:
-            numbertrié.pop(0)
-            numbertrié.append(14)
-        for i in range(len(numbertrié)-1):
-            compt = 0
-            nbrange = 5
-            if len(numbertrié)-i < 5:
-                nbrange = len(numbertrié)-i
-            for a in range(nbrange-1):
-                if numbertrié[i+1+a] - numbertrié[i+a] == 1:
+        if tab:
+            for x in tab:
+                numbertrié.pop(x)
+        if numbertrié[-1] == 1:
+            numbertrié.insert(0, 14)
+        for i in range(len(numbertrié)):
+            if i == 0:
+                continue
+            for y in range(len(numbertrié)):
+                if numbertrié[y] - numbertrié[i] == 1:
+                    suite.append(numbertrié[y])
                     compt += 1
-                else:
-                    continue
+                    break
             if compt == 4:
                 suite.append(numbertrié[i])
-        if len(suite) > 0:
-            return suite[len(suite)-1] + 63
-        else:
-            return 0
-
+                suite.sort()
+                res = suite[4]
+                if suite[4] == 14:
+                    suite[4] = "as"
+                return res + 63, suite, f"Vous avez une suite! Commençant par un {suite[0]} et finissant par un {suite[4]}"
+        return None
 
     def Couleur(self, player):
         number = self.NumberCheck(self.CheckCards(player))
         power = self.PowerCheck(self.CheckCards(player))
-        tabpower = power
         coul = False
         for i in range(len(power)):
             couleur = []
             couleury = []
+            nbcolor = []
             for y in range(len(power)):
-                if tabpower[i] == tabpower[y] and i != y:
-                    couleur.append(tabpower[y])
+                if power[i] == power[y] and i != y:
+                    couleur.append(power[y])
                     couleury.append(y)
+                    nbcolor.append(number[y])
                 if len(couleur) == 4:
-                    couleur.append(tabpower[i])
+                    nbcolor.append(number[i])
+                    couleur.append(power[i])
                     couleury.append(i)
                     coul = True
                     break
-            if coul == True:
+            if coul:
                 break
+        nbcolor.sort()
         couleur.sort()
         couleury.sort()
         tabnb = []
@@ -224,14 +150,14 @@ class EndGame:
             for x in couleury:
                 tabnb.append(number[x])
             tabnb.sort()
+            res = tabnb[4]
             if tabnb[0] == 1:
-                tabnb[4] = 14
-            return tabnb[4] + 72
-        else:
-            return 0
+                res = 14
+            return res + 72, nbcolor, "Vous avez une couleur!", couleur[0]
+        return None
 
-    def Carre(self,player):
-        number = [1,1,1,4,3,9,1]
+    def Carre(self, player):
+        number = self.NumberCheck(self.CheckCards(player))
         nbcarre = number
         nbcarre.sort()
         carre = []
@@ -248,18 +174,25 @@ class EndGame:
             if poped == True:
                 break
         if len(carre) == 1:
+            res = carre[0]
             if carre[0] == 1:
-                carre[0] = 14
-            return carre[0] + 50
-        else:
-            return 0
+                res = 14
+            return (res + 50), carre[0], (f"Vous avez un carré de {carre[0]}")
+        return None
 
-    #def QuinteFlush(self):
+    def QuinteFlush(self, player):
+        Couleur = self.Couleur(player)
+        if Couleur:
+            Suite = self.Suite(player, Couleur[1])
+            if Suite:
+                return Couleur[0] + 9, Suite[1], f"Vous avez une Quinte Flush de {Couleur[3]}s!"
+        return None
 
-    # def QuinteFlushRoyale(self) int:
-    # for a in len(number):
-
-    # return 89
+    def QuinteFlushRoyale(self, player):
+        QuinteFlush = self.QuinteFlush(player)
+        if QuinteFlush == 94:
+            return 94, QuinteFlush[1], f"Vous avez une Quinte Flush Royale"
+        return None
 
     # * COMBINAISONS :
 
